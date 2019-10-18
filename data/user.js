40828468c89f0659f5abbda633da0fef757bd863
Qianxiong Xu <20086454@mail.wit.ie>
2019-10-18 16:50:01 +0100

    Mock the data of user

---
+++ data/user.js
@@ -0,0 +1,29 @@
+/**
+ * @Author: Sam
+ * @Date: 2019/10/18
+ * @Version: 1.0
+ **/
+const users = [
+    {
+        "username": "1007824874",
+        "password": "yyq19981212",
+        "phone": "353894889596",
+        "address": [
+            "APARTMENT 19, BLOCK 2, RIVERWALK, INNER RING ROAD, WATERFORD, IRELAND"
+        ],
+        "pay": [
+            4319472635034936
+        ]
+    },
+    {
+        "username": "xqx",
+        "password": "yyq19981212",
+        "phone": "353894889596",
+        "address": [
+            "APARTMENT 19, BLOCK 2, RIVERWALK, INNER RING ROAD, WATERFORD, IRELAND"
+        ],
+        "pay": [
+            6228480395827429378
+        ]
+    }
+]
