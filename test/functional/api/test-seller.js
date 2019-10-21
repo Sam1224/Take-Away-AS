9d66757cc9d067a3d5e9553a32893fb9bef301c3
Qianxiong Xu <20086454@mail.wit.ie>
2019-10-21 22:56:16 +0100

    Test PUT /seller/:id when there is no jwt token

--- test/functional/api/test-seller.js
+++ test/functional/api/test-seller.js
@@ -335,4 +335,40 @@ describe('Seller', () => {
             })
         })
     })
+
+    describe('PUT /seller/:id', () => {
+        describe('when there is no jwt token', () => {
+            it('should require to login if it does not have a jwt token', () => {
+                let seller = {}
+                seller.name = 'test1'
+                seller.description = 'Meituan Delivery'
+                seller.deliveryTime = 40
+                seller.bulletin = 'Test 1'
+                seller.supports = [{
+                    'type': 1,
+                    'description': 'VC orange juice 80% discount'
+                }]
+                seller.avatar = 'http://static.galileo.xiaojukeji.com/static/tms/seller_avatar_256px.jpg'
+                seller.pics = [
+                    "http://fuss10.elemecdn.com/8/71/c5cf5715740998d5040dda6e66abfjpeg.jpeg?imageView2/1/w/180/h/180"
+                ]
+                seller.infos = [
+                    "Invoice supported, please fill in the invoice title when ordered",
+                    "Class: Other cuisine, porridge store",
+                    "1340, Unit 102, Block B, bottom business, longguan real estate building, Western Huilongguan Street, Changping, Beijing",
+                    "Opening hours: 10:00-20:30"
+                ]
+                setTimeout(() => {
+                    return request(server)
+                        .put(`/seller/${validID}`)
+                        .send(seller)
+                        .expect(200)
+                        .then((res) => {
+                            expect(res.body.code).to.equal(1)
+                            expect(res.body.message).equals("Not Login Yet, Please Login")
+                        })
+                }, 1000)
+            })
+        })
+    })
 })
