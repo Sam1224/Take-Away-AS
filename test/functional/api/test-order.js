a048639ec73da8f61d1d389231e0c4984065497c
Qianxiong Xu <20086454@mail.wit.ie>
2019-10-22 16:27:18 +0100

    Initialize a MongoMemoryServer for testing and update scripts in package.json

---
+++ test/functional/api/test-order.js
@@ -0,0 +1,64 @@
+/**
+ * @Author: Sam
+ * @Date: 2019/10/22
+ * @Version: 1.0
+ **/
+const chai = require('chai')
+const expect = chai.expect
+const request = require('supertest')
+const jwt = require('jsonwebtoken')
+
+const config = require('../../../config')
+
+// mongod-memory-server
+const MongoMemoryServer = require('mongodb-memory-server').MongoMemoryServer
+const Order = require('../../../models/order')
+const {MongoClient} = require('mongodb')
+
+const _ = require('lodash')
+let server
+let mongod
+let db, validID
+let url, connection, collection
+
+// jwt parameters
+let username = "admin"
+let token
+let superSecret = config.superSecret
+
+describe('Order', () => {
+    before(async () => {
+        mongod = new MongoMemoryServer({
+            instance: {
+                port: 27017,
+                dbPath: './test/database',
+                dbName: 'takeawayapp'
+            }
+        })
+        // Async Trick - this ensures the database is created before
+        // we try to connect to it or start the server
+        url = await mongod.getConnectionString()
+        connection = await MongoClient.connect(url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        db = connection.db(await mongod.getDbName())
+        collection = db.collection('order')
+        server = require('../../../bin/www')
+
+        token = jwt.sign({username: username}, superSecret, {
+            // 1 hour
+            expiresIn: 3600
+        })
+    })
+
+    after(async () => {
+        try {
+            await connection.close()
+            await mongod.stop()
+            await server.close()
+        } catch (err) {
+            console.log(err)
+        }
+    })
+})
