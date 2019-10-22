e345f2b92b57e9c0cefe403e244bf3bf3c400ede
Qianxiong Xu <20086454@mail.wit.ie>
2019-10-22 20:48:28 +0100

    Test GET /order/user/:id when there is no jwt token

--- test/functional/api/test-order.js
+++ test/functional/api/test-order.js
@@ -177,4 +177,25 @@ describe('Order', () => {
             })
         })
     })
+
+    describe('GET /order/user/:id', () => {
+        describe('when there is no jwt token', () => {
+            it('should require to login if it does not have a jwt token', () => {
+                let order = {}
+                return request(server)
+                    .get('/order/user/user2')
+                    .set("Accept", "application/json")
+                    .expect("Content-Type", /json/)
+                    .send(order)
+                    .expect(200)
+                    .then((res) => {
+                        expect(res.body.code).to.equal(1)
+                        expect(res.body.message).equals("Not Login Yet, Please Login")
+                    })
+                    .catch((err) => {
+                        console.log(err)
+                    })
+            })
+        })
+    })
 })
