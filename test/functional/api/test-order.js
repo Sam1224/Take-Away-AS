46dd4a9a875b4e731bf7ffa0f8f99a6d5bd6595a
Qianxiong Xu <20086454@mail.wit.ie>
2019-10-22 21:48:21 +0100

    Test DELETE /order/:id when there is no jwt token

--- test/functional/api/test-order.js
+++ test/functional/api/test-order.js
@@ -551,4 +551,25 @@ describe('Order', () => {
             })
         })
     })
+
+    describe('DELETE /order/:id', () => {
+        describe('when there is no jwt token', () => {
+            it('should require to login if it does not have a jwt token', () => {
+                let order = {}
+                return request(server)
+                    .delete(`/order/${validID}`)
+                    .set("Accept", "application/json")
+                    .expect("Content-Type", /json/)
+                    .send(order)
+                    .expect(200)
+                    .then((res) => {
+                        expect(res.body.code).to.equal(1)
+                        expect(res.body.message).equals("Not Login Yet, Please Login")
+                    })
+                    .catch((err) => {
+                        console.log(err)
+                    })
+            })
+        })
+    })
 })
