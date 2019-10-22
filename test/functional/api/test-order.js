8657cd7fceb242daa8fe00ef852290b3a882ad21
Qianxiong Xu <20086454@mail.wit.ie>
2019-10-22 20:49:39 +0100

    Test GET /order/user/:id when there is an invalid token

--- test/functional/api/test-order.js
+++ test/functional/api/test-order.js
@@ -197,5 +197,26 @@ describe('Order', () => {
                     })
             })
         })
+        describe('when there is a jwt token', () => {
+            describe('when the token is invalid', () => {
+                it('should return an invalid error', () => {
+                    let order = {}
+                    order.token = "123"
+                    return request(server)
+                        .get('/order/user/user2')
+                        .send(order)
+                        .set("Accept", "application/json")
+                        .expect("Content-Type", /json/)
+                        .expect(200)
+                        .then((res) => {
+                            expect(res.body.code).to.equal(-1)
+                            expect(res.body.error.name).equals("JsonWebTokenError")
+                        })
+                        .catch((err) => {
+                            console.log(err)
+                        })
+                })
+            })
+        })
     })
 })
