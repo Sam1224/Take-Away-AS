115e11f319d714f9acc498509b18211ec250b55e
Qianxiong Xu <20086454@mail.wit.ie>
2019-10-22 14:53:16 +0100

    Test DELETE /user/address when there is an invalid token

--- test/functional/api/test-user.js
+++ test/functional/api/test-user.js
@@ -510,5 +510,28 @@ describe('User', () => {
                     })
             })
         })
+        describe('when there is a jwt token', () => {
+            describe('when the token is invalid', () => {
+                it('should return an invalid error', () => {
+                    let user = {}
+                    user.token = "123"
+                    user.username = "test1"
+                    user.address = "APARTMENT 19, BLOCK 2, RIVERWALK, INNER RING ROAD, WATERFORD, IRELAND"
+                    return request(server)
+                        .delete('/user/address')
+                        .send(user)
+                        .set("Accept", "application/json")
+                        .expect("Content-Type", /json/)
+                        .expect(200)
+                        .then((res) => {
+                            expect(res.body.code).to.equal(-1)
+                            expect(res.body.error.name).equals("JsonWebTokenError")
+                        })
+                        .catch((err) => {
+                            console.log(err)
+                        })
+                })
+            })
+        })
     })
 })
